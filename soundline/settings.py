9c0d4d9e95f2defa352cd74d3560d13935f09430
Misha4231 <123841126+Misha4231@users.noreply.github.com>
2023-04-08 15:58:03 +0200

    Update settings.py

--- soundline/settings.py
+++ soundline/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ik)mpl_07cxhtn)!qbx4ujsk+k@jzq$#j=$#ao1b15x5pejh@'
+SECRET_KEY = 'django-insecure-*ik)mpl_07cxh76rytchttdyhjtrfsy5trhjzydju5pejh@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
